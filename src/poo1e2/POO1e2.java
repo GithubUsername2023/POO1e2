a4cf040a901116f78369405762665894eeb242c7
Martin Selis <98183961+martinselis1978@users.noreply.github.com>
2023-04-26 00:12:06 -0300

    POO ejercicio 2

---
+++ src/poo1e2/POO1e2.java
@@ -0,0 +1,15 @@
+package poo1e2;
+
+import Entidad.Circuferencia;
+import java.util.Scanner;
+
+public class POO1e2 {
+
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Ingrese el radio de un círculo");
+        Circuferencia crear=new Circuferencia(leer.nextDouble());
+        System.out.println("El radio del circulo es "+crear.getCircunferencia()+", el perimetro del circulo es "+crear.getPerimetro()+", y su área es "+", "+crear.getArea());
+    }
+    
+}
